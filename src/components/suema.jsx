d28ab0c4eff6a70002dde68e59635411cb40aaab
lhaavel <lukas.haavel@gmail.com>
2024-08-21 19:00:02 +0300

    mida vittu kinda prod vennad

--- src/components/suema.jsx
+++ src/components/suema.jsx
@@ -1,7 +1,44 @@
-import { createElement } from './../framework';
+import { createElement, useState } from './../framework';
 let count = 0;
-export const Suema = () => 
-<div className="ye">
-    yoyoyo
-</div>
-     
+let amogus = true;
+
+
+
+
+export const Suema = () => {
+  const [state, setState] = useState(1)
+
+  const incrementCount = () => setState(c => c + 1);
+  const resetCount = () => setState(c =>c= 0);
+    
+    return (
+        <div className="ye">
+            yoyoyooo
+            <Counter count={state} incrementCount={incrementCount}/>
+            <ResetCounter resetCount={resetCount}/>
+        </div>
+        )
+    }
+export function Counter({count, incrementCount}) {
+        return (
+            <div>
+              <h1 onClick={incrementCount}>
+                Count: {count}
+              </h1>
+            </div>
+        )
+       }
+
+export function ResetCounter({ resetCount }) {
+  return (
+    <div>
+        <button onClick={resetCount}>ResetAlumine</button>
+    </div>
+);
+}
+
+{/* <style>
+button {
+    color: red
+}
+</style> */}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { render } from './framework';
 import App from './App';
+import './App.css';
 
 const rootElement = document.getElementById("root");
 render(App, rootElement);
